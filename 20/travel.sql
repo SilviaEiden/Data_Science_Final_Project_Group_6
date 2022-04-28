0dcb8fea2daf559b4187a68ad0ba4aa33b9e65fd
Hina Midnight <hinamidnight@hinas-imac.myfiosgateway.com>
2022-04-27 20:46:21 -0400

    Commit

---
+++ 20/travel.sql
@@ -0,0 +1,30 @@
+--Creating table for Travel Agency
+CREATE TABLE Travel (
+	"Agency" VARCHAR,
+	"Agency Type" VARCHAR,
+	"Distibution Channel" VARCHAR,
+	"Product Name" VARCHAR,
+	"Claim" VARCHAR,
+	"Duration" INT, 
+	"Destination" VARCHAR, 
+	"Net Sales" INT, 
+	"Commission (in value)" VARCHAR, 
+	"Gender" VARCHAR,
+	"AGE" INT
+);
+--Creating table for Travel Agency
+CREATE TABLE Traveling (
+	"Agency" VARCHAR,
+	"Agency Type" VARCHAR,
+	"Distibution Channel" VARCHAR,
+	"Product Name" VARCHAR,
+	"Claim" VARCHAR,
+	"Duration" INT, 
+	"Destination" VARCHAR, 
+	"Net Sales" VARCHAR, 
+	"Commission (in value)" VARCHAR, 
+	"Gender" VARCHAR,
+	"AGE" INT
+);
+
+SELECT * FROM Traveling;
