4108ac988b47a9623e8ff17141406f844d47d2be
hinaem <95388367+hinaem@users.noreply.github.com>
2022-04-30 21:42:28 -0400

    Cleaned SQL file

---
+++ travel.sql
@@ -0,0 +1,14 @@
+--Creating table for Travel Agency cleaned
+CREATE TABLE TRAVEL (
+	"Agency" VARCHAR,
+	"Agency Type" VARCHAR,
+	"Distibution Channel" VARCHAR,
+	"Product Name" VARCHAR,
+	"Claim" INT,
+	"Duration" INT, 
+	"Destination" VARCHAR, 
+	"Net Sales" VARCHAR, 
+	"Commission (in value)" VARCHAR, 
+	"AGE" INT
+);
+SELECT * FROM TRAVEL;
